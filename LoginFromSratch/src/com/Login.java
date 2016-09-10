09064791f8b1b703747db31d7e7e30821fc21036
Shkelqim1410 <shkelqim.mula1410@gmail.com>
2016-09-10 19:45:48 +0200

    Creating the LoginFromSratch project, easy 2 frame project

---
+++ LoginFromSratch/src/com/Login.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author Shkelqim
+ */
+public class Login extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+       Parent root = FXMLLoader.load(getClass().getResource("/com/Login.fxml"));
+       
+       Scene scene = new Scene(root);
+       
+       primaryStage.setScene(scene);
+       primaryStage.setTitle("Login");
+       primaryStage.show();
+    }
+    
+    
+}
---
+++ LoginFromSratch/src/com/LoginController.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com;
+
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXPasswordField;
+import com.jfoenix.controls.JFXTextField;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+/**
+ * FXML Controller class
+ *
+ * @author Shkelqim
+ */
+public class LoginController implements Initializable {
+
+    @FXML
+    private JFXTextField txtUsername;
+    @FXML
+    private JFXPasswordField txtPassword;
+    @FXML
+    private JFXButton btnLogin;
+    @FXML
+    private Label outMessage;
+
+    /**
+     * Initializes the controller class.
+     * @param url
+     * @param rb
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void btnLoginAction(ActionEvent event) throws Exception {
+        if(txtUsername.getText().equals("test") && txtPassword.getText().equals("test")) {
+            Parent parent = FXMLLoader.load(getClass().getResource("/com/Main.fxml"));
+            Stage stage = new Stage();
+            Scene scene = new Scene(parent);
+            stage.setScene(scene);
+            stage.setTitle("Main Frame");
+            stage.show();
+        }
+        else {
+            outMessage.setText("Username or Password Invalid!");
+        }
+    }
+    
+}
